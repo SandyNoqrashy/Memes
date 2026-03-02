0871277ac4d95ad8b190d13eb9f54b6cdd5f921d
Your Name <you@example.com>
2026-03-02 16:51:24 +0200

    test

---
+++ Js/Js.js
@@ -0,0 +1,24 @@
+let input = document.querySelector("input");
+let btnShow = document.querySelector("button");
+let myMeme = document.querySelector(".mymeme");
+
+btnShow.addEventListener("click", () => {
+  let show = input.value;
+  if (show >= 0 && show < 100)
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then((res) => res.json())
+      .then((data) => {
+        let meme = data.data.memes[show];
+        myMeme.innerHTML = `
+        <h2 class="text-danger">${meme.name}</h2>
+    <div class="mx-auto border rounded p-3 text-center "  style="max-width: 400px; max-height: 450px; " >
+    <img src="${meme.url}"class="img-fluid" style="max-height:300px; object-fit:contain;" >
+    </div>
+    `;
+   
+
+      });
+       else{
+        myMeme.innerHTML=`<h2 class="text-danger">Please Enter Valid Number</h2>`
+    }
+});
